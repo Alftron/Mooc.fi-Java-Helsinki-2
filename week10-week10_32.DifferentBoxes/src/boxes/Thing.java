7d3b1a058547639a700900401388ee3604b38d8d
Alftron <mja.haycock@gmail.com>
2019-10-08 08:42:26 +0100

    Completed Different Boxes assignement

--- week10-week10_32.DifferentBoxes/src/boxes/Thing.java
+++ week10-week10_32.DifferentBoxes/src/boxes/Thing.java
@@ -8,7 +8,11 @@ public class Thing {
     public Thing(String name, int weight) {
 
         this.name = name;
-        this.weight = weight;
+        if (weight >= 0) {
+            this.weight = weight;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Thing(String name) {
@@ -21,6 +25,24 @@ public class Thing {
 
     public int getWeight() {
         return weight;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        Thing other = (Thing)obj;
+        
+        return this.name.equals(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
     }
 
 }
