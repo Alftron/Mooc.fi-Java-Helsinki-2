8e46404416986b339fb4040d4ab27bc61eb5dea9
Alftron <mja.haycock@gmail.com>
2019-10-09 15:57:12 +0100

    Tiny bit of work on Dungeon assignement

--- week10-week10_33.Dungeon/src/dungeon/Dungeon.java
+++ week10-week10_33.Dungeon/src/dungeon/Dungeon.java
@@ -6,8 +6,9 @@ public class Dungeon {
     private int length;
     private int height;
     private int vampires;
-    private int moves;
     private boolean vampiresMove;
+   
+    private Player player;
     
     public Dungeon(int length, int height, int vampires, int moves, boolean vampiresMove) {
         /*
@@ -19,8 +20,8 @@ public class Dungeon {
             this.length = length;
             this.height = height;
             this.vampires = vampires;
-            this.moves = moves;
-            this.vampiresMove = vampiresMove;   
+            this.vampiresMove = vampiresMove;
+            player = new Player(moves);
         }
     }
     
--- week10-week10_33.Dungeon/src/dungeon/Player.java
+++ week10-week10_33.Dungeon/src/dungeon/Player.java
@@ -3,6 +3,12 @@ package dungeon;
 
 public class Player {
     
+    private int remainingMoves;
+    
+    public Player(int moves) {
+        remainingMoves = moves;
+    }
+    
     @Override
     public String toString() {
         return "@ " + "x " + "y";
