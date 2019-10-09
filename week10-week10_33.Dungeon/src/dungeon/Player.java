4f54a63d6e46bb4ebcbf397793c834c3b5329969
Alftron <mja.haycock@gmail.com>
2019-10-09 15:52:28 +0100

    Tiny bit of work on Dungeon assignement

--- week10-week10_33.Dungeon/src/dungeon/Player.java
+++ week10-week10_33.Dungeon/src/dungeon/Player.java
@@ -3,4 +3,9 @@ package dungeon;
 
 public class Player {
     
+    @Override
+    public String toString() {
+        return "@ " + "x " + "y";
+    }
+    
 }
--- week10-week10_33.Dungeon/src/dungeon/Vampire.java
+++ week10-week10_33.Dungeon/src/dungeon/Vampire.java
@@ -3,4 +3,9 @@ package dungeon;
 
 public class Vampire {
     
+    @Override
+    public String toString() {
+        return "v " + "x " + "y";
+    }
+    
 }
